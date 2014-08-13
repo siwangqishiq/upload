321af139d5cb7978da0536ead5b887471a5ad076
siwangqishiq <525647740@qq.com>
2014-08-13 22:39:16 +0800

    init1
    init1

---
+++ day01/FirstDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("你好 世界~~\nHello World!\n");
+            Console.ReadKey();
+            Console.WriteLine("我也是HelloWorld!\n");
+            Console.ReadKey();
+        }//end Main
+    }//end class
+
+    class Demo2
+    {
+        public void show()
+        {
+            Console.WriteLine("你好 世界!");
+            Console.ReadKey();
+        }
+    }
+}//end namespace
---
+++ day01/MySecondDemo2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MySecondDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("你好 世界~~\nHello World!\n");
+            Console.ReadKey();
+            Console.WriteLine("我也是HelloWorld!\n");
+            Console.ReadKey();
+        }
+    }//end class
+}//end namespace
